ba6fb977aaf67ab3fbcafc28526690059517bfc0
Syed Abbas Ahmad <syedabbasahmad6@gmail.com>
2025-10-25 15:57:40 +0500

    Build out ssfm section 02

--- src/nlse_ssfm/ssfm.py
+++ src/nlse_ssfm/ssfm.py
@@ -6,3 +6,11 @@ Symmetric split-step Fourier solver for the Nonlinear Schrodinger Equation (NLSE
 
     i du/dxi + (s/2) d^2u/dtau^2 + N^2|u|^2 u = 0
 
+Convention: Agrawal (Nonlinear Fiber Optics, 6th ed.)
+    s = -sign(beta2) = +1 for anomalous dispersion
+    N^2 = gamma * P0 * L_D (soliton number squared)
+"""
+
+import numpy as np
+
+
