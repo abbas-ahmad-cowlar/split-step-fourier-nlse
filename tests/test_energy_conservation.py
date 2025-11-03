ac61fb094731bbfcd2b7d8139f086c06abe16450
Syed Abbas Ahmad <syedabbasahmad6@gmail.com>
2025-11-03 09:08:33 +0500

    Build out test energy conservation section 01

---
+++ tests/test_energy_conservation.py
@@ -0,0 +1,8 @@
+"""Energy conservation check for N=1 and N=2 solitons."""
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from nlse_ssfm.nlse_utils import create_grid, sech_pulse, compute_energy
+from nlse_ssfm.ssfm import ssfm_propagate
+
